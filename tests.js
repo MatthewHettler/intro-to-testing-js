9d8b4ba9e4732623f006fc96d8c97e9df3f1a2f3
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2019-12-06 11:20:40 -0600

    Edited exercise 10 tests

--- tests.js
+++ tests.js
@@ -45,13 +45,4 @@ describe('isFive', function () {
     it('should return true when 5 is passed', function() {
         expect(isFive(5)).toBe(true);
     });
-    // it('should return the string "Hello, Alex!" when executed', function() {
-    //     expect(sayHello("Alex")).toBe("Hello, Alex!");
-    // });
-    // it('should return the string "Hello, Pat!" when executed', function() {
-    //     expect(sayHello("Pat")).toBe("Hello, Pat!");
-    // });
-    // it('should return the string "Hello, World!" when executed', function() {
-    //     expect(sayHello(false)).toBe("Hello, World!");
-    // });
 });
